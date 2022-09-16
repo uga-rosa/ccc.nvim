f37e3d33a87f092d08ffcb154de5a697bc9f06b7
uga-rosa <uga6603@gmail.com>
2022-09-17 07:06:49 +0900

    fix: corrected internal representation of CMYK

--- lua/ccc/input/cmyk.lua
+++ lua/ccc/input/cmyk.lua
@@ -4,22 +4,22 @@ local utils = require("ccc.utils")
 ---@class HslInput: ColorInput
 local CmykInput = setmetatable({
     name = "CMYK",
-    max = { 100, 100, 100, 100 },
+    max = { 1, 1, 1, 1 },
     min = { 0, 0, 0, 0 },
-    delta = { 1, 1, 1, 1 },
+    delta = { 0.005, 0.005, 0.005, 0.005 },
     bar_name = { "C", "M", "Y", "K" },
 }, { __index = ColorInput })
 
----@param v number
+---@param n number
 ---@return string
-function CmykInput.format(v)
-    return ("%5d%%"):format(v)
+function CmykInput.format(n)
+    return ("%5.1f%%"):format(n * 100)
 end
 
 ---@param x number
 ---@return number
 local function div255(x)
-    return x / 2.55
+    return x / 255
 end
 
 ---@param RGB integer[]
@@ -27,13 +27,13 @@ end
 function CmykInput.from_rgb(RGB)
     local C, M, Y, K
     local R_, G_, B_ = unpack(vim.tbl_map(div255, RGB))
-    K = 100 - utils.max(R_, G_, B_)
-    if K == 100 then
-        return { 0, 0, 0, 100 }
+    K = 1 - utils.max(R_, G_, B_)
+    if K == 1 then
+        return { 0, 0, 0, 1 }
     end
-    C = (100 - R_ - K) / (100 - K) * 100
-    M = (100 - G_ - K) / (100 - K) * 100
-    Y = (100 - B_ - K) / (100 - K) * 100
+    C = (1 - R_ - K) / (1 - K)
+    M = (1 - G_ - K) / (1 - K)
+    Y = (1 - B_ - K) / (1 - K)
     return { C, M, Y, K }
 end
 
@@ -42,12 +42,12 @@ end
 function CmykInput.to_rgb(CMYK)
     local R, G, B
     local C, M, Y, K = unpack(CMYK)
-    if K == 100 then
+    if K == 1 then
         return { 0, 0, 0 }
     end
-    R = utils.round(0.0255 * (100 - C) * (100 - K))
-    G = utils.round(0.0255 * (100 - M) * (100 - K))
-    B = utils.round(0.0255 * (100 - Y) * (100 - K))
+    R = utils.round(255 * (1 - C) * (1 - K))
+    G = utils.round(255 * (1 - M) * (1 - K))
+    B = utils.round(255 * (1 - Y) * (1 - K))
     return { R, G, B }
 end
 
