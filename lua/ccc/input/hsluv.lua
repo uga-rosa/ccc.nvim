b175d3ff534c9e40e47266cfbebedb98dd119aca
uga-rosa <uga6603@gmail.com>
2022-09-18 04:10:42 +0900

    fix: HSLuv

--- lua/ccc/input/hsluv.lua
+++ lua/ccc/input/hsluv.lua
@@ -4,11 +4,21 @@ local convert = require("ccc.utils.convert")
 ---@class HSLuvInput: ColorInput
 local HSLuvInput = setmetatable({
     name = "HSLuv",
-    max = { 360, 100, 100 },
+    max = { 360, 1, 1 },
     min = { 0, 0, 0 },
-    delta = { 1, 1, 1 },
+    delta = { 1, 0.01, 0.01 },
     bar_name = { "H", "S", "L" },
 }, { __index = ColorInput })
+
+---@param n number
+---@param i integer
+---@return string
+function HSLuvInput.format(n, i)
+    if i > 1 then
+        n = n * 100
+    end
+    return ("%6d"):format(n)
+end
 
 ---@param RGB number[]
 ---@return number[] HSLuv
