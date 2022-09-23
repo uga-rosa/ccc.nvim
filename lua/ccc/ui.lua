d8bddbcedba7f45b880bcba3ef1dd5c466ad0653
uga-rosa <uga6603@gmail.com>
2022-09-23 17:53:21 +0900

    fix: highlight

--- lua/ccc/ui.lua
+++ lua/ccc/ui.lua
@@ -79,7 +79,7 @@ function UI:_open()
     api.nvim_win_set_option(
         self.win_id,
         "winhl",
-        "NormalFloat:CccFloatNormal,FloatBorder:CccFloatBorder"
+        "Normal:CccFloatNormal,EndOfBuffer:CccFloatNormal,FloatBorder:CccFloatBorder"
     )
 end
 
