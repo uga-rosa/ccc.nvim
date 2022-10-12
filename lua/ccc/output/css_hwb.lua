9ee884ff131ab9b0a57f7a03ebf69f8a8b9327a2
uga-rosa <uga6603@gmail.com>
2022-10-13 05:29:26 +0900

    feat: add output/css_hwb

---
+++ lua/ccc/output/css_hwb.lua
@@ -0,0 +1,24 @@
+local convert = require("ccc.utils.convert")
+
+---@class CssHwbOutput: ColorOutput
+local CssHwbOutput = {
+    name = "CssHWB",
+}
+
+---@param RGB RGB
+---@param A? Alpha
+---@return string
+function CssHwbOutput.str(RGB, A)
+    local H, W, B = unpack(convert.rgb2hwb(RGB))
+    W = W * 100
+    B = B * 100
+    if A then
+        local pattern = "hwb(%d %d%% %d%% / %d%%)"
+        return pattern:format(H, W, B, A * 100)
+    else
+        local pattern = "hwb(%d %d%% %d%%)"
+        return pattern:format(H, W, B)
+    end
+end
+
+return CssHwbOutput
