d7c2ed07d45c6616211cbbce27c33403a4f72f0b
uga-rosa <uga6603@gmail.com>
2022-10-13 05:41:22 +0900

    feat: add output/css_oklch

---
+++ lua/ccc/output/css_oklch.lua
@@ -0,0 +1,23 @@
+local convert = require("ccc.utils.convert")
+
+---@class CssOklchOutput: ColorOutput
+local CssOklchOutput = {
+    name = "CssOKLCH",
+}
+
+---@param RGB RGB
+---@param A? Alpha
+---@return string
+function CssOklchOutput.str(RGB, A)
+    local L, C, H = unpack(convert.rgb2oklch(RGB))
+    L = L * 100
+    if A then
+        local pattern = "oklab(%d%% %d %d / %d%%)"
+        return pattern:format(L, C, H, A * 100)
+    else
+        local pattern = "oklab(%d%% %d %d)"
+        return pattern:format(L, C, H)
+    end
+end
+
+return CssOklchOutput
