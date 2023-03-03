71ae9d6e52d5045d7c5b419be881d9651de074e9
delphinus <me@delphinus.dev>
2023-03-03 14:58:40 +0900

    feat: clean up logic to work with special chars

--- lua/ccc/config/default.lua
+++ lua/ccc/config/default.lua
@@ -71,6 +71,7 @@ return {
       "[%w_]{{pattern}}",
       "{{pattern}}[%w_]",
     },
+    custom_entries = nil,
   },
   ---@type hl_mode
   highlight_mode = "bg",
--- lua/ccc/picker/custom_entries.lua
+++ lua/ccc/picker/custom_entries.lua
@@ -6,34 +6,45 @@ local parse = require("ccc.utils.parse")
 ---@field rgb { [string]: integer[] }
 ---@field re { match_str: function } vim.regex instance
 ---@field min_length integer
+---@field color_table { [string]: string }
+---@field exclude_pattern_option string[]|string
 local CustomEntries = {}
 
 ---@param color_table { [string]: string }
 ---@return CustomEntries
 CustomEntries.new = function(color_table)
-  local self = { rgb = {} }
-  ---@type string[]
-  local names = {}
-  for name, rgb in pairs(color_table) do
+  return setmetatable(
+    { color_table = color_table, rgb = {}, pattern = {}, exclude_pattern = {} },
+    { __index = CustomEntries }
+  )
+end
+
+function CustomEntries:init()
+  if self.re then
+    return
+  end
+  ---@type { plain: string, vim: string }[]
+  local patterns = {}
+  for name, rgb in pairs(self.color_table) do
     local r, g, b = rgb:match("(%x%x)(%x%x)(%x%x)")
     if r then
-      table.insert(names, name)
+      local escaped = name:gsub([[\]], "%0%0")
+      table.insert(patterns, { plain = name, vim = escaped })
       self.rgb[name] = { parse.hex(r), parse.hex(g), parse.hex(b) }
     end
   end
-  table.sort(names, function(a, b)
-    return #a > #b
+  -- Sort names to match the longest word at first.
+  table.sort(patterns, function(a, b)
+    return #a.plain > #b.plain
   end)
-  self.min_length = #names[#names]
-  self.pattern = { [[\<]] .. table.concat(names, [[\|]]) .. [[\>]] }
-  self.re = vim.regex(self.pattern[1])
-  local ex_pat = config.get("exclude_pattern")
-  self.exclude_pattern = utils.expand_template(ex_pat.custom_table, self.pattern)
-  return setmetatable(self, { __index = CustomEntries })
+  self.min_length = #patterns[#patterns].plain
+  local vim_patterns = vim.tbl_map(function(v)
+    return v.vim
+  end, patterns)
+  local pattern = [[\V\<\(]] .. table.concat(vim_patterns, [[\|]]) .. [[\)\>]]
+  self.exclude_pattern_option = config.get("exclude_pattern").custom_entries
+  self.re = vim.regex(pattern) --[[@as { match_str: function }]]
 end
-
--- dummy
-function CustomEntries:init() end
 
 ---@param s string
 ---@param init? integer
@@ -42,6 +53,7 @@ function CustomEntries:init() end
 ---@return number[]? RGB
 ---@return number? alpha
 function CustomEntries:parse_color(s, init)
+  self:init()
   init = vim.F.if_nil(init, 1) --[[@as integer]]
   local target = s:sub(init)
   if #target < self.min_length then
@@ -54,7 +66,9 @@ function CustomEntries:parse_color(s, init)
     if rgb then
       local start = target_start + init
       local end_ = target_end + init - 1
-      if not utils.is_excluded(self.exclude_pattern, s, init, start, end_) then
+      local escaped = name:gsub("[$^()%%.%[%]*+-?]", "%%%0")
+      local exclude_pattern = utils.expand_template(self.exclude_pattern_option, { escaped })
+      if not utils.is_excluded(exclude_pattern, s, init, start, end_) then
         return start, end_, rgb
       end
     end
