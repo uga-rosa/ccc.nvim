a0cbf4409cc91dd85dcef03e7c43d16808181985
uga-rosa <uga6603@gmail.com>
2024-02-15 01:02:53 +0900

    public api

--- lua/ccc/init.lua
+++ lua/ccc/init.lua
@@ -1,202 +1,84 @@
-local UI = require("ccc.ui")
-local config = require("ccc.config")
-
-local M = {
-  input = {
-    rgb = require("ccc.input.rgb"),
-    hsl = require("ccc.input.hsl"),
-    hwb = require("ccc.input.hwb"),
-    lab = require("ccc.input.lab"),
-    lch = require("ccc.input.lch"),
-    oklab = require("ccc.input.oklab"),
-    oklch = require("ccc.input.oklch"),
-    cmyk = require("ccc.input.cmyk"),
-    hsluv = require("ccc.input.hsluv"),
-    okhsl = require("ccc.input.okhsl"),
-    hsv = require("ccc.input.hsv"),
-    okhsv = require("ccc.input.okhsv"),
-    xyz = require("ccc.input.xyz"),
-  },
-  output = {
-    hex = require("ccc.output.hex"),
-    hex_short = require("ccc.output.hex_short"),
-    css_rgb = require("ccc.output.css_rgb"),
-    css_hsl = require("ccc.output.css_hsl"),
-    css_hwb = require("ccc.output.css_hwb"),
-    css_lab = require("ccc.output.css_lab"),
-    css_lch = require("ccc.output.css_lch"),
-    css_oklab = require("ccc.output.css_oklab"),
-    css_oklch = require("ccc.output.css_oklch"),
-    float = require("ccc.output.float"),
-  },
-  picker = {
-    css_rgb = require("ccc.picker.css_rgb"),
-    css_name = require("ccc.picker.css_name"),
-    hex = require("ccc.picker.hex"),
-    css_hsl = require("ccc.picker.css_hsl"),
-    css_hwb = require("ccc.picker.css_hwb"),
-    css_lab = require("ccc.picker.css_lab"),
-    css_lch = require("ccc.picker.css_lch"),
-    css_oklab = require("ccc.picker.css_oklab"),
-    css_oklch = require("ccc.picker.css_oklch"),
-    custom_entries = require("ccc.picker.custom_entries"),
-    ansi_escape = require("ccc.picker.ansi_escape"),
-    trailing_whitespace = require("ccc.picker.trailing_whitespace"),
-    trailing_space = function(opts)
-      vim.notify("Incorrect picker name: `trailing_space`\nCorrect to `trailing_whitespace`")
-      return require("ccc.picker.trailing_whitespace")(opts)
-    end,
-  },
-  mapping = {},
-}
-
-setmetatable(M, {
-  __index = function(self, key)
-    if key == "inputs" or key == "outputs" or key == "pickers" then
-      local properer_key = key:sub(1, -2)
-      vim.notify(("ccc.%s is deprecated. Use ccc.%s instead."):format(key, properer_key), vim.log.levels.WARN)
-      return self[properer_key]
+local function loader(root)
+  return function(self, key)
+    local modname = table.concat({ "ccc", root, key }, ".")
+    local ok, module = pcall(require, modname)
+    if not ok then
+      vim.notify("Unknown module: " .. modname)
+      return
     end
-  end,
-})
-
----@param opt? table
-function M.setup(opt)
-  vim.validate({ opt = { opt, "t", true } })
-  config.setup(opt or {})
+    rawset(self, key, module)
+    return module
+  end
 end
 
----@param b_color CccColor
----@param a_color CccColor
----@param width integer
----@return string line
----@return integer b_start_col
----@return integer b_end_col
----@return integer a_start_col
----@return integer a_end_col
-function M.output_line(b_color, a_color, width)
-  local b_hex = b_color:hex()
-  local a_str = a_color:str()
-  local line = b_hex .. " =>" .. string.rep(" ", width - #b_hex - #a_str - 3) .. a_str
+---@param opts ccc.Options
+local function setup(opts)
+  require("ccc.config").setup(opts)
+  opts = require("ccc.config").options
 
-  local b_start_col = 0
-  local b_end_col = #b_hex
-  local a_start_col = width - #a_str
-  local a_end_col = -1
-  return line, b_start_col, b_end_col, a_start_col, a_end_col
+  if opts.lsp then
+    require("ccc.handler.lsp"):enable()
+  end
+
+  local core = require("ccc.core").new()
+  vim.api.nvim_create_user_command("CccPick", function()
+    core:pick()
+  end, {})
+  vim.keymap.set("i", "<Plug>(ccc-insert)", function()
+    core:insert()
+  end)
+
+  vim.keymap.set("o", "<Plug>(ccc-select-color)", function()
+    require("ccc.select").select("v")
+  end)
+  vim.keymap.set("x", "<Plug>(ccc-select-color)", function()
+    require("ccc.select").select("o")
+  end)
+
+  vim.api.nvim_create_user_command("CccConvert", function()
+    require("ccc.convert").toggle()
+  end, {})
+
+  local highlighter = require("ccc.highlighter")
+  highlighter:init()
+
+  vim.api.nvim_create_user_command("CccHighlighterEnable", function(opt)
+    local bufnr = tonumber(opt.args)
+    highlighter:enable(bufnr)
+  end, { nargs = "?" })
+  vim.api.nvim_create_user_command("CccHighlighterDisable", function(opt)
+    local bufnr = tonumber(opt.args)
+    highlighter:disable(bufnr)
+  end, { nargs = "?" })
+  vim.api.nvim_create_user_command("CccHighlighterToggle", function(opt)
+    local bufnr = tonumber(opt.args)
+    highlighter:toggle(bufnr)
+  end, { nargs = "?" })
+
+  if opts.highlighter.auto_enable then
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = vim.api.nvim_create_augroup("ccc-highlighter-auto-enable", {}),
+      callback = function(ev)
+        local ok, stat = pcall(vim.loop.fs_stat, ev.file)
+        if ok and stat and stat.size > opts.highlighter.max_byte then
+          return
+        end
+        highlighter:enable(ev.buf)
+      end,
+    })
+  end
 end
 
----@param delta integer
-function M.delta(delta)
-  UI:delta(delta)
-end
-
----@param percent integer
-function M.set_percent(percent)
-  UI:set_percent(percent)
-end
-
-function M.mapping.quit()
-  vim.cmd("quit")
-end
-
-function M.mapping.complete()
-  UI:complete()
-end
-
-function M.mapping.toggle_input_mode()
-  UI:toggle_input_mode()
-end
-
-function M.mapping.toggle_output_mode()
-  UI:toggle_output_mode()
-end
-
-function M.mapping.reset_mode()
-  UI:reset_mode()
-end
-
-function M.mapping.show_alpha()
-  UI.alpha:show()
-end
-
-function M.mapping.hide_alpha()
-  UI.alpha:hide()
-end
-
-function M.mapping.toggle_alpha()
-  UI.alpha:toggle()
-end
-
-function M.mapping.show_prev_colors()
-  UI.prev_colors:show()
-end
-
-function M.mapping.hide_prev_colors()
-  UI.prev_colors:hide()
-end
-
-function M.mapping.toggle_prev_colors()
-  UI.prev_colors:toggle()
-end
-
-function M.mapping.goto_prev()
-  UI.prev_colors:goto_prev()
-end
-
-function M.mapping.goto_next()
-  UI.prev_colors:goto_next()
-end
-
-function M.mapping.goto_head()
-  UI.prev_colors:goto_head()
-end
-
-function M.mapping.goto_tail()
-  UI.prev_colors:goto_tail()
-end
-
-function M.mapping.increase1()
-  M.delta(1)
-end
-
-function M.mapping.increase5()
-  M.delta(5)
-end
-
-function M.mapping.increase10()
-  M.delta(10)
-end
-
-function M.mapping.decrease1()
-  M.delta(-1)
-end
-
-function M.mapping.decrease5()
-  M.delta(-5)
-end
-
-function M.mapping.decrease10()
-  M.delta(-10)
-end
-
-function M.mapping.set0()
-  M.set_percent(0)
-end
-
-function M.mapping.set50()
-  M.set_percent(50)
-end
-
-function M.mapping.set100()
-  M.set_percent(100)
-end
-
-function M.mapping.click()
-  UI:click()
-end
-
--- Disable only some of the default mappings
-M.mapping.none = "<Plug>(ccc-none)"
-
-return M
+return {
+  input = setmetatable({}, {
+    __index = loader("input"),
+  }),
+  output = setmetatable({}, {
+    __index = loader("output"),
+  }),
+  picker = setmetatable({}, {
+    __index = loader("picker"),
+  }),
+  setup = setup,
+  mapping = require("ccc.mapping"),
+}
