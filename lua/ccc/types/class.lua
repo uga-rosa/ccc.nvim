bfd70cf7936b8e64bec64898ef8ee0b32af5c65f
uga-rosa <uga6603@gmail.com>
2024-02-15 01:03:08 +0900

    class

--- lua/ccc/types/class.lua
+++ lua/ccc/types/class.lua
@@ -1,6 +1,33 @@
----@class ColorOutput
+---@class ccc.UI
+---@field bufnr integer
+---@field winid integer
+---@field ns_id integer
+---@field color ccc.Color
+---@field show_prev_colors boolean
+---@field before_color ccc.Color
+---@field prev_colors ccc.PrevColors
+---@field is_quit boolean
+---@field on_quit_callback? function
+---@field new fun(): ccc.UI
+---@field open fun(self: ccc.UI, color: ccc.Color, prev_colors: ccc.PrevColors)
+---@field update fun(self: ccc.UI)
+---@field close fun(self: ccc.UI)
+---@field on_close fun(self: ccc.UI)
+---@field reset_view fun(self: ccc.UI)
+---@field point_at fun(self: ccc.UI): ccc.UI.point
+---@field set_point fun(self: ccc.UI, point: ccc.UI.point)
+
+---@class ccc.UI.point
+---@field type "none" | "color" | "alpha" | "prev"
+---@field index integer
+
+---@class ccc.ColorOutput
 ---@field name string
 ---@field str fun(RGB: number[], A?: number): string
 
----@class ColorPicker
+---@class ccc.ColorPicker
 ---@field parse_color fun(self, s: string, init?: integer, bufnr?: integer): start: integer?, end_: integer?, RGB?, Alpha?, vim.api.keyset.highlight?
+
+---@class ccc.hl_info
+---@field range lsp.Range
+---@field hl_name string
