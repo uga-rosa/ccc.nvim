1af4a246b85219d04e20625d792502e7d47cc37a
uga-rosa <uga6603@gmail.com>
2024-02-15 01:01:58 +0900

    convert

--- lua/ccc/convert.lua
+++ lua/ccc/convert.lua
@@ -1,40 +1,26 @@
-local api = vim.api
+local M = {}
 
-local config = require("ccc.config")
-local utils = require("ccc.utils")
+function M.toggle()
+  local opts = require("ccc.config").options
 
----@class Convert
----@field convert {[1]: ColorPicker, [2]: ColorOutput}[]
-local Convert = {}
-
-function Convert:init()
-  if self.convert ~= nil then
-    return
-  end
-  self.convert = config.get("convert")
-end
-
-function Convert:toggle()
-  self:init()
-  local line = api.nvim_get_current_line()
-  local cursor_col = utils.col()
-  for _, v in ipairs(self.convert) do
-    local picker, output = unpack(v)
+  local line = vim.api.nvim_get_current_line()
+  local cursor = require("ccc.utils").cursor()
+  local row, cursor_col = cursor[1] - 1, cursor[2]
+  for _, v in ipairs(opts.convert) do
+    local picker, output = v[1], v[2]
 
     local init = 1
-    local start, end_, rgb, alpha
     while true do
-      start, end_, rgb, alpha = picker:parse_color(line, init)
-      if start == nil then
+      local start_col, end_col, rgb, alpha = picker:parse_color(line, init)
+      if not (start_col and end_col and rgb) then
         break
-      elseif start <= cursor_col and cursor_col <= end_ then
-        local new_line = line:sub(1, start - 1) .. output.str(rgb, alpha) .. line:sub(end_ + 1)
-        api.nvim_set_current_line(new_line)
-        return
+      elseif start_col <= cursor_col and end_col >= cursor_col then
+        vim.api.nvim_buf_set_text(0, row, start_col - 1, row, end_col, { output.str(rgb, alpha) })
+      else
+        init = end_col + 1
       end
-      init = end_ + 1
     end
   end
 end
 
-return Convert
+return M
