ef7d27ee0a0fda113b1010a150ed853e2f9314e1
uga-rosa <uga6603@gmail.com>
2024-02-25 21:42:33 +0900

    Add test for :CccConvert

--- lua/ccc/convert.lua
+++ lua/ccc/convert.lua
@@ -1,11 +1,15 @@
+local api = require("ccc.utils.api")
+
 local Convert = {}
 
 function Convert.toggle()
   local opts = require("ccc.config").options
 
   local line = vim.api.nvim_get_current_line()
-  local cursor = require("ccc.utils").cursor()
-  local row, cursor_col = cursor[1] - 1, cursor[2]
+  local row, cursor_col = api.get_cursor()
+  -- to 1-indexed column
+  cursor_col = cursor_col + 1
+
   for _, v in ipairs(opts.convert) do
     local picker, output = v[1], v[2]
 
